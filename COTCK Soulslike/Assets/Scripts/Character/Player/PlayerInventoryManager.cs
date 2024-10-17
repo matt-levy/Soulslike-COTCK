ca53783a841f8cbbafe2a6a6f9c03f19061aca8a
MTocci1 <Mason.Tocci1@marist.edu>
2024-10-17 14:24:15 -0400

    added plant ally with almost full functionality

--- COTCK Soulslike/Assets/Scripts/Character/Player/PlayerInventoryManager.cs
+++ COTCK Soulslike/Assets/Scripts/Character/Player/PlayerInventoryManager.cs
@@ -6,4 +6,26 @@ public class PlayerInventoryManager : CharacterInventoryManager
 {
     public WeaponItem currentRightHandWeapon;
     public WeaponItem currentLeftHandWeapon;
+
+    public ColorPassive currentColorPassive;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ActivateColorPassive();
+        }
+    }
+
+    public void ActivateColorPassive()
+    {
+        if (currentColorPassive != null)
+        {
+            if (currentColorPassive is GreenColorPassive greenColorPassive)
+            {
+                greenColorPassive.SummonPlantAlly(gameObject.transform);
+            }
+        }
+    }
 }
